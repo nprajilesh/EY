1213a79f79d3257924d9cd361d682a5e9eea6c75
nprajilesh <nprajilesh@gmail.com>
2014-07-10 15:45:18 +0530

    'data base insertion and fetching included'

--- test/index.js
+++ test/index.js
@@ -8,9 +8,13 @@ var stops={};
 var places={};
 var markers=[];
 var pos;
-google.maps.event.addDomListener(window, 'load', initialize);
+var cordinates=[];
 var map;
+var polystr;
+google.maps.event.addDomListener(window, 'load', initialize);
 
+
+/* Initializes the Google map and load the input div in top left direction services are initialized */
 function initialize() {
 
   var mapOptions = {
@@ -22,19 +26,12 @@ function initialize() {
       position: google.maps.ControlPosition.RIGHT_BOTTOM
     }
   }
-
- angular.module('plunker', ['ui.bootstrap']);
- var PopoverDemoCtrl = function ($scope) {
-  $scope.dynamicPopover = 'Hello, World!';
-  $scope.dynamicPopoverTitle = 'Title';
- };
-
   map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
   
   if(navigator.geolocation) 
     navigator.geolocation.getCurrentPosition(function(position) {
-      pos = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-      map.setCenter(pos);
+            pos = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+            map.setCenter(pos);
     });
   else{
     pos = new google.maps.LatLng(8.487495,76.948623);   // Browser doesn't support Geolocation
@@ -69,8 +66,8 @@ function search(elem){
 
   });
   
-  var place;
-  google.maps.event.addListener(autocomplete, 'place_changed', function() {
+   var place;
+    google.maps.event.addListener(autocomplete, 'place_changed', function() {
     infowindow.close();
     marker.setVisible(false);
     place = autocomplete.getPlace();
@@ -84,12 +81,10 @@ function search(elem){
     var regex= /^waypoint-?/;
     
     if(regex.test(elem.id)){
-      console.log("matches");
       waypoints.push({
           location:place.formatted_address,
           stopover:false
       });
-      console.log(waypoints);
       count++;
       document.getElementById('waypoint_button').disabled=false;
       if(count===8)
@@ -116,10 +111,11 @@ function search(elem){
     }
 
     infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-    //infowindow.open(map, marker);
   });
 }
 
+
+/* Route details are obtained by direction Service API*/
 function getdirections(){
   
   var request = {
@@ -138,22 +134,24 @@ function getdirections(){
       document.getElementById('reset_button').style.visibility="visible";
       places['source'].marker.setMap(null);
       places['destination'].marker.setMap(null);
+      inserttodb(response);
     }
   });
 }
 
+/* Upto 8 wavepoints are added for each wavepoint a new autocomplete input field is created*/
 function addwaypoint(elem){
   
   if(count<8){
-    console.log(count);
     document.getElementById(elem.id).disabled=true;
     $("#form>input:nth-child("+inputc+")").after('<br><input id="waypoint-'+count+'" type="text" class="controls" placeholder="Via" onclick="search(this)" size="45">');
     inputc++;
     inputc++;
-    console.log(inputc);
   }
 }
 
+
+/* Stops are added into maps by searching in autocomplete field*/
 function addstops(elem){
   var input = document.getElementById(elem.id);
   var autocomplete = new google.maps.places.Autocomplete(input);
@@ -166,7 +164,7 @@ function addstops(elem){
   });
 
   google.maps.event.addListener(autocomplete, 'place_changed', function() {
-    place = autocomplete.getPlace();
+    var place = autocomplete.getPlace();
     marker.setVisible(false);
 
     if (!place.geometry)
@@ -177,13 +175,12 @@ function addstops(elem){
 
     marker.setPosition(place.geometry.location);
     marker.setVisible(true);
-
-  });
+    });
 }
 
+/* Resets the markers and Wave Points*/
 function reset_route(){
   directionsDisplay.setMap(null);
-  console.log(directionsDisplay.getMap());
   places={};
   waypoints=[];
   for(var i=0; i<markers.length; i++)
@@ -192,4 +189,50 @@ function reset_route(){
   document.getElementById("reset_button").style.visibility="hidden";
   map.setCenter(pos);
   map.setZoom(10);
+}
+
+
+/* Overview_polyline is inserted into databse along with route Id*/
+function inserttodb(response){
+    
+    $.ajax({
+      type: 'POST',
+      url: 'http://192.168.0.175:1337/shapes/create',
+      data: {shape_id:'1',route:response.routes[0].overview_polyline},
+      dataType: 'json',
+      success: function(data) { 
+        alert('Route Created Sucessfully '); 
+      },
+      error: function() { alert('something bad happened'); }
+    });
+}
+
+
+/*Load Route into map , overview_polyline is fetched from data base and decoded to get the latLngs*/
+function loadroute(){
+  $.ajax({
+      type: 'POST',
+      url: 'http://192.168.0.175:1337/shapes/findbyid',
+      data: {id:2},
+      dataType: 'json',
+      success: function(data) { 
+       
+        console.log(data);
+        var decpoly = google.maps.geometry.encoding.decodePath(data.route);
+        console.log(decpoly);
+        var polyline= new google.maps.Polyline({map:map,path:decpoly}); 
+        // and this is where we actually draw it. 
+        var polyOptions = { 
+          path: pathCoordinates, 
+          strokeColor: '#ff0000', 
+          strokeOpacity: 1.0, 
+          strokeWeight: 1 
+        };
+        poly = new google.maps.Polyline(polyOptions); 
+        poly.setMap(map);
+        console.log(data); 
+      },
+      error: function() { alert('something bad happened'); }
+    });
+    
 }
